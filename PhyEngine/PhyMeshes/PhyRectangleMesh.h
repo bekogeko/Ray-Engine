dd55e1f3aaf5ee44c14dc5758502555833056724
Bekir Gulestan <gulestanbekir@gmail.com>
2024-03-17 02:58:17 -0500

    hotfix: const function

--- PhyEngine/PhyMeshes/PhyRectangleMesh.h
+++ PhyEngine/PhyMeshes/PhyRectangleMesh.h
@@ -13,7 +13,7 @@ public:
     PhyRectangleMesh(float width, float height);
 
     // used for board collision detection
-    Rectangle GetBoundingBox(Vector2 position, float rotation) override;
+    Rectangle GetBoundingBox(Vector2 position, float rotation) const override;
 
     std::vector<std::tuple<Vector2, Vector2>> GetFaces() const override;
 
